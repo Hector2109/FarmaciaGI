dc5e7702ffa22e498b837fbf966eeb7a372869a9
Enrique Rodríguez Angulo <enrique.rodriguez246966@potros.itson.edu.mx>
2024-03-18 14:09:25 -0700

    Clase Venta
    Se creó la clase venta

---
+++ FarmaciaGIDominio/src/main/java/org/itson/diseñosoftware/farmaciagidominio/Venta.java
@@ -0,0 +1,61 @@
+package org.itson.diseñosoftware.farmaciagidominio;
+
+import java.util.GregorianCalendar;
+import org.itson.diseñosoftware.farmaciagipersistencia.Productos;
+/**
+ *
+ * @author Enrique Rodriguez
+ */
+public class Venta {
+    private Integer idVenta;
+    private Productos productos;
+    private Float total;
+    private GregorianCalendar fecha;
+
+    public Venta(Integer idVenta, Productos productos, Float total, GregorianCalendar fecha) {
+        this.idVenta = idVenta;
+        this.productos = productos;
+        this.total = total;
+        this.fecha = fecha;
+    }
+    
+    public Venta(Productos productos, Float total, GregorianCalendar fecha) {
+        this.productos = productos;
+        this.total = total;
+        this.fecha = fecha;
+    }
+
+    public Integer getIdVenta() {
+        return idVenta;
+    }
+
+    public void setIdVenta(Integer idVenta) {
+        this.idVenta = idVenta;
+    }
+
+    public Productos getProductos() {
+        return productos;
+    }
+
+    public void setProductos(Productos productos) {
+        this.productos = productos;
+    }
+
+    public Float getTotal() {
+        return total;
+    }
+
+    public void setTotal(Float total) {
+        this.total = total;
+    }
+
+    public GregorianCalendar getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(GregorianCalendar fecha) {
+        this.fecha = fecha;
+    }
+    
+    
+}
