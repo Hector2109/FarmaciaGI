2cfb2323093f0d2d4df1e6ed81ad7ff7b4cdff63
imnotrichi <116768255+imnotrichi@users.noreply.github.com>
2024-04-25 16:37:34 -0700

    cambios en los DTOs

--- FarmaciaGI_dtos/src/main/java/org/itson/disenosoftware/farmaciagi_dtos/VentaDTO.java
+++ FarmaciaGI_dtos/src/main/java/org/itson/disenosoftware/farmaciagi_dtos/VentaDTO.java
@@ -1,20 +1,21 @@
 package org.itson.disenosoftware.farmaciagi_dtos;
 
-import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 import java.util.List;
 
 public class VentaDTO {
 
     private String codigo;
     private List<ProductoDTO> productos;
+    private List<PromocionDTO> promociones;
     private Float total;
     private Calendar fecha;
     
     public VentaDTO(String codigo){
         this.codigo = codigo;
-        this.productos = new ArrayList<>();
+        this.productos = new LinkedList<>();
         this.total = 0.0F;
         this.fecha = null;
     }
@@ -26,12 +27,28 @@ public class VentaDTO {
         this.fecha = fecha;
     }
 
+    public VentaDTO(String codigo, List<ProductoDTO> productos, List<PromocionDTO> promociones, Float total, Calendar fecha) {
+        this.codigo = codigo;
+        this.productos = productos;
+        this.promociones = promociones;
+        this.total = total;
+        this.fecha = fecha;
+    }
+
     public String getCodigo() {
         return codigo;
     }
 
     public List<ProductoDTO> getProductos() {
         return productos;
+    }
+    
+    public List<PromocionDTO> getPromociones(){
+        return promociones;
+    }
+    
+    public void setPromociones(List<PromocionDTO> promociones){
+        this.promociones = promociones;
     }
 
     public Float getTotal() {
