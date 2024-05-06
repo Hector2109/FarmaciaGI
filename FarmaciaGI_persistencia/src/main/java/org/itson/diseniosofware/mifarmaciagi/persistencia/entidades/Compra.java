9700b07f3a890e4b9e9be4078f3fd4c8a75efb6c
Enrique Rodríguez Angulo <enrique.rodriguez246966@potros.itson.edu.mx>
2024-05-06 00:09:51 -0700

    Clases Compra y Clase entidad compra
    Se implementaron diferentes clases ademas de terminar la clase entidad de compras

---
+++ FarmaciaGI_persistencia/src/main/java/org/itson/diseniosofware/mifarmaciagi/persistencia/entidades/Compra.java
@@ -0,0 +1,127 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.itson.diseniosofware.mifarmaciagi.persistencia.entidades;
+
+import java.time.Instant;
+import java.util.List;
+import java.util.Objects;
+import org.bson.types.ObjectId;
+
+/**
+ *
+ * @author Enrique Rodriguez
+ */
+public class Compra {
+
+    private ObjectId _id;
+    private List<Producto> productos;
+    private List<Proveedor> proveedores;
+    private Float costoTotal;
+    private Instant fecha;
+
+    public Compra() {
+    }
+
+    public Compra(List<Producto> productos, List<Proveedor> proveedores, Float costoTotal, Instant fecha) {
+        this.productos = productos;
+        this.proveedores = proveedores;
+        this.costoTotal = costoTotal;
+        this.fecha = fecha;
+    }
+    
+    public ObjectId getId() {
+        return _id;
+    }
+
+    public void setId(ObjectId _id) {
+        this._id = _id;
+    }
+
+    public List<Producto> getProductos() {
+        return productos;
+    }
+
+    public void setProductos(List<Producto> productos) {
+        this.productos = productos;
+    }
+
+    public List<Proveedor> getProveedores() {
+        return proveedores;
+    }
+
+    public void setProveedores(List<Proveedor> proveedores) {
+        this.proveedores = proveedores;
+    }
+
+    public Float getCostoTotal() {
+        return costoTotal;
+    }
+
+    public void setCostoTotal(Float costoTotal) {
+        this.costoTotal = costoTotal;
+    }
+
+    public Instant getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Instant fecha) {
+        this.fecha = fecha;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Compra{");
+        sb.append("_id=").append(_id);
+        sb.append(", productos=").append(productos);
+        sb.append(", proveedores=").append(proveedores);
+        sb.append(", costoTotal=").append(costoTotal);
+        sb.append(", fecha=").append(fecha);
+        sb.append('}');
+        return sb.toString();
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 43 * hash + Objects.hashCode(this._id);
+        hash = 43 * hash + Objects.hashCode(this.productos);
+        hash = 43 * hash + Objects.hashCode(this.proveedores);
+        hash = 43 * hash + Objects.hashCode(this.costoTotal);
+        hash = 43 * hash + Objects.hashCode(this.fecha);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final Compra other = (Compra) obj;
+        if (!Objects.equals(this._id, other._id)) {
+            return false;
+        }
+        if (!Objects.equals(this.productos, other.productos)) {
+            return false;
+        }
+        if (!Objects.equals(this.proveedores, other.proveedores)) {
+            return false;
+        }
+        if (!Objects.equals(this.costoTotal, other.costoTotal)) {
+            return false;
+        }
+        return Objects.equals(this.fecha, other.fecha);
+    }
+    
+    
+    
+}
