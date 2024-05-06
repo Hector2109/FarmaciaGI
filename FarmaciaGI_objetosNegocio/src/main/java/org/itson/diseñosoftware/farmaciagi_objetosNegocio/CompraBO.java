e9d20583b333a415aca64cc066962f8420f3021c
Enrique Rodríguez Angulo <enrique.rodriguez246966@potros.itson.edu.mx>
2024-05-06 14:59:28 -0700

    Métodos clase CompraBO
    Método de la clase CompraBO para convertir valores DTO a valores POJO, ademas se terminó el método para registrar una compra en la bd

--- FarmaciaGI_objetosNegocio/src/main/java/org/itson/diseñosoftware/farmaciagi_objetosNegocio/CompraBO.java
+++ FarmaciaGI_objetosNegocio/src/main/java/org/itson/diseñosoftware/farmaciagi_objetosNegocio/CompraBO.java
@@ -5,6 +5,8 @@
 package org.itson.diseñosoftware.farmaciagi_objetosNegocio;
 
 import java.time.Instant;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -14,7 +16,12 @@ import org.itson.diseniosofware.mifarmaciagi.persistencia.Exception.Persistencia
 import org.itson.diseniosofware.mifarmaciagi.persistencia.daos.ComprasDAO;
 import org.itson.diseniosofware.mifarmaciagi.persistencia.daos.IComprasDAO;
 import org.itson.diseniosofware.mifarmaciagi.persistencia.entidades.Compra;
+import org.itson.diseniosofware.mifarmaciagi.persistencia.entidades.Direccion;
+import org.itson.diseniosofware.mifarmaciagi.persistencia.entidades.Producto;
+import org.itson.diseniosofware.mifarmaciagi.persistencia.entidades.Proveedor;
 import org.itson.disenosoftware.farmaciagi_dtos.CompraDTO;
+import org.itson.disenosoftware.farmaciagi_dtos.ProductoDTO;
+import org.itson.disenosoftware.farmaciagi_dtos.ProveedorDTO;
 import org.itson.diseñosoftware.farmaciagi_objetosNegocio.excepciones.ObjetosNegocioException;
 
 /**
@@ -37,14 +44,14 @@ public class CompraBO {
             codigo = generarCodigoCompra();
             compra = encontrarVenta(codigo);
         } while (compra != null);
-        
+
         compra = new Compra();
         compra.setCodigo(codigo);
         compra.setCostoTotal(compraNueva.getCostoTotal());
         long millis = compraNueva.getFecha().getTimeInMillis();
         compra.setFecha(Instant.ofEpochMilli(millis));
-        //compra.setProveedores(compraNueva.getProveedores());
-        //compra.setProductos(compraNueva.getProductos());
+        compra.setProveedores(proveedoresDTOAProveedoresPOJO(compraNueva.getProveedores()));
+        compra.setProductos(productosDTOAProductosPOJO(compraNueva.getProductos()));
 
         try {
             comprasDAO.registrar(compra);
@@ -71,8 +78,59 @@ public class CompraBO {
         }
         return codigo.toString();
     }
+
+    /**
+     * Permite convertir una lista de Producto DTO a Producto POJO
+     *
+     * @param productosDTO La lista de Producto DTO
+     * @return La lista de Producto POJO
+     */
+    private List<Producto> productosDTOAProductosPOJO(List<ProductoDTO> productosDTO) {
+
+        List<Producto> productos = new LinkedList<>();
+
+        for (ProductoDTO productoDTO : productosDTO) {
+            Producto producto = new Producto();
+            producto.setCantidad(productoDTO.getCantidad());
+            producto.setNombre(productoDTO.getNombre());
+            producto.setMarca(productoDTO.getMarca());
+            producto.setCodigo(productoDTO.getCodigo());
+            producto.setCosto(productoDTO.getCosto());
+            productos.add(producto);
+        }
+        return productos;
+    }
     
         /**
+     * Permite convertir una lista de Proveedores DTO a Proveedores POJO
+     *
+     * @param proveedoresDTO La lista de Proveedor DTO
+     * @return La lista de Proveedor POJO
+     */
+    private List<Proveedor> proveedoresDTOAProveedoresPOJO(List<ProveedorDTO> proveedoresDTO) {
+
+        List<Proveedor> proveedores = new LinkedList<>();
+
+        for (ProveedorDTO proveedorDTO : proveedoresDTO) {
+            Direccion direccion = new Direccion();
+            direccion.setCalle(proveedorDTO.getDireccion().getCalle());
+            direccion.setCiudad(proveedorDTO.getDireccion().getCiudad());
+            direccion.setCodigo_postal(proveedorDTO.getDireccion().getCodigo_postal());
+            direccion.setColonia(proveedorDTO.getDireccion().getColonia());
+            direccion.setNumero(proveedorDTO.getDireccion().getNumero());
+            
+            Proveedor proveedor = new Proveedor();
+            
+            proveedor.setNombre(proveedorDTO.getNombre());
+            proveedor.setRfc(proveedorDTO.getRfc());
+            proveedor.setTelefonos(proveedorDTO.getTelefonos());
+            proveedor.setDireccion(direccion);
+
+        }
+        return proveedores;
+    }
+
+    /**
      * Método el cual encuentra una venta a partir del código
      *
      * @param codigo código de venta
